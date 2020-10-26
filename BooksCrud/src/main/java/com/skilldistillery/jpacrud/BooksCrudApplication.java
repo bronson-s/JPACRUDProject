9175a0bf0304a37c34a82026847d825cf410ecfd
Bronson Spies <spiesbronson@gmail.com>
2020-10-26 10:13:38 -0600

    deployment proccess

--- BooksCrud/src/main/java/com/skilldistillery/jpacrud/BooksCrudApplication.java
+++ BooksCrud/src/main/java/com/skilldistillery/jpacrud/BooksCrudApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.jpacrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BooksCrudApplication {
+public class BooksCrudApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(BooksCrudApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksCrudApplication.class, args);
